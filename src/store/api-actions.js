36d7adef50f75bb602d60e84859e102c85f12f7e
Егор Тумаков <el.tumakov@gmail.com>
2020-11-01 19:12:42 +0300

    Добавляет новое действие для перенаправлений

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,4 +1,4 @@
-import {loadQuestions, requireAuthorization} from "./action";
+import {loadQuestions, requireAuthorization, redirectToRoute} from "./action";
 import {AuthorizationStatus} from "../const";
 
 export const fetchQuestionList = () => (dispatch, _getState, api) => (
@@ -17,4 +17,5 @@ export const checkAuth = () => (dispatch, _getState, api) => (
 export const login = ({login: email, password}) => (dispatch, _getState, api) => (
   api.post(`/login`, {email, password})
     .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+    .then(() => dispatch(redirectToRoute(`/result`)))
 );
