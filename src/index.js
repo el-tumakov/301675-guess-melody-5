eb4eda8611f9a9ea8e6b3afb20fca30cde312d55
Егор Тумаков <el.tumakov@gmail.com>
2020-11-01 18:05:51 +0300

    Подключает redux-thunk при конфигурации хранилища
    - получает с сервера сисок вопросов для игр

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import {createAPI} from "./services/api";
 import App from "./components/app/app";
 import rootReducer from "./store/reducers/root-reducer";
+import {fetchQuestionList} from "./store/api-actions";
+
+const api = createAPI();
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
+
+store.dispatch(fetchQuestionList());
 
 ReactDOM.render(
     <Provider store={store}>
