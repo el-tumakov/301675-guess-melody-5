e6ff50470f465dbdf379c163520d4565ed311e24
Егор Тумаков <el.tumakov@gmail.com>
2020-11-11 13:07:51 +0300

    Добавляет enzyme тесты

---
+++ src/components/auth-screen/auth-screen.e2e.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {AuthScreen} from "./auth-screen";
+import {noop} from "../../utils";
+
+configure({adapter: new Adapter()});
+
+it(`Should replay button be pressed`, () => {
+  const handleReplayButtonClick = jest.fn();
+
+  const wrapper = shallow(
+      <AuthScreen
+        onReplayButtonClick={handleReplayButtonClick}
+        onSubmit={noop}
+      />
+  );
+
+  const replayButton = wrapper.find(`button.replay`);
+
+  replayButton.simulate(`click`);
+  expect(handleReplayButtonClick).toHaveBeenCalledTimes(1);
+});
---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import WelcomeScreen from "./welcome-screen";
+
+
+configure({adapter: new Adapter()});
+
+it(`Should welcome button be pressed`, () => {
+  const handlePlayButtonClick = jest.fn();
+
+  const welcomeScreen = shallow(
+      <WelcomeScreen
+        errorsCount={3}
+        onPlayButtonClick={handlePlayButtonClick}
+      />
+  );
+
+  const welcomeButton = welcomeScreen.find(`button.welcome__button`);
+
+  welcomeButton.simulate(`click`);
+
+  expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
+});
